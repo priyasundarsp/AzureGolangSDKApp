4b8038acd2ce1f156675f5aef0b0b5d55274d960
Aravind Raj <aravinu19@gmail.com>
2022-10-19 21:58:57 +0530

    added styling

--- webui/src/components/hero/index.js
+++ webui/src/components/hero/index.js
@@ -1,28 +1,37 @@
 import { h } from 'preact';
 
-const Hero = () => (
-    <section className='section'>
-        <div className='is-flex-direction-column is-justify-content-center is-align-items-center'>
-            <p className='title is-family-sans-serif has-text-weight-normal has-text-centered'>
-                Simple Azure Resource Management
-            </p>
-            <p className='subtitle has-text-centered mb-6'>
-                A sample Golang SDK application to manage Azure resources
-            </p>
+const lcols = {
+    width: "95%",
+    maxWidth: "500px",
+    minWidth: "180px"
+};
 
-            <div className='is-flex is-flex-direction-row is-justify-content-center is-align-items-center'>
-                <div className='card p-6 is-flex' style={"max-width: fit-content;"}>
-                    <div className='card-image'>
-                        <figure class="image">
-                            <img src='/assets/gophers.png' style={"max-width: 450px!important; width: 98%; height: auto;"} />
-                        </figure>
-                    </div>
-                </div>
+const rcols = {
+    width: "95%",
+    maxWidth: "550px",
+    minWidth: "180px"
+};
+
+
+const Hero = () => (
+    <section className='section is-justify-content-center is-align-items-center bg mb-6'>
+        <div className='is-flex is-flex-direction-row my-6 is-flex-wrap-wrap is-justify-content-center is-align-items-center'>
+            <div className='is-flex' style={lcols}>
+
+                <img src='/assets/gophers.png' style={"max-width: 400px!important; width: 98%; height: auto;"} />
             </div>
-            <div className='container is-flex-direction-row is-justify-content-center is-align-items-center is-flex mt-6'>
-                <p className='has-text-centered' style={{ maxWidth: "550px" }}>
-                    This Sample App provides basic interface to list, create and delete the CosmosDB database accounts, storage accounts, VNet's and static web apps.
+            <div className='is-flex is-flex-direction-column my-6 is-justify-content-center' style={rcols}>
+                <p className='title is-family-sans-serif has-text-weight-normal has-text-centered'>
+                    Simple Azure Resource Management
                 </p>
+                <p className='subtitle has-text-centered is-family-secondary my-2'>
+                    A sample Golang SDK application to manage Azure resources
+                </p>
+                <div className='container is-flex-direction-row is-justify-content-center is-align-items-center is-flex mt-6'>
+                    <p className='has-text-centered is-family-secondary has-text-weight-normal' style={{ maxWidth: "550px" }}>
+                        This Sample App provides basic interface to list, create and delete the CosmosDB database accounts, storage accounts, VNet's and static web apps.
+                    </p>
+                </div>
             </div>
         </div>
     </section>
