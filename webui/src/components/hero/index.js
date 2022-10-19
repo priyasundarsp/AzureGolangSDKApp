17d3ab2d9ab224d180040cb73b92a3200a6c9c46
Aravind Raj <aravinu19@gmail.com>
2022-10-19 20:29:46 +0530

    updated header

--- webui/src/components/hero/index.js
+++ webui/src/components/hero/index.js
@@ -1,14 +1,24 @@
 import { h } from 'preact';
 
 const Hero = () => (
-    <section className='hero is-medium my-6' style={"background-image: linear-gradient(45deg, #00cdac 38%, #8ddad5 70%); border-raduis: 19px!important;"}>
-        <div className='hero-body is-flex-direction-column is-justify-content-center is-align-items-center'>
+    <section className='section'>
+        <div className='is-flex-direction-column is-justify-content-center is-align-items-center'>
             <p className='title is-family-sans-serif has-text-weight-normal has-text-centered'>
                 Simple Azure Resource Management
             </p>
-            <p className='subtitle has-text-centered'>
+            <p className='subtitle has-text-centered mb-6'>
                 A sample Golang SDK application to manage Azure resources
             </p>
+
+            <div className='is-flex is-flex-direction-row is-justify-content-center is-align-items-center'>
+                <div className='card p-6 is-flex' style={"max-width: fit-content;"}>
+                    <div className='card-image'>
+                        <figure class="image">
+                            <img src='/assets/gophers.png' style={"max-width: 450px!important; width: 98%; height: auto;"} />
+                        </figure>
+                    </div>
+                </div>
+            </div>
             <div className='container is-flex-direction-row is-justify-content-center is-align-items-center is-flex mt-6'>
                 <p className='has-text-centered' style={{ maxWidth: "550px" }}>
                     This Sample App provides basic interface to list, create and delete the CosmosDB database accounts, storage accounts, VNet's and static web apps.
